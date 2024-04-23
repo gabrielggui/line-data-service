4b507ee170302dd9535e0b3dc27d40c467dff9ed
Gabriel Guilherme <gabriel.guilherme@compasso.com.br>
2024-04-23 02:36:32 -0300

    refactor: Refactor method to throw InvalidStatusLinhaException instead of IllegalArgumentException

--- src/main/java/br/com/telefonica/ms/linedataservice/enums/StatusLinhaEnum.java
+++ src/main/java/br/com/telefonica/ms/linedataservice/enums/StatusLinhaEnum.java
@@ -1,20 +1,23 @@
 package br.com.telefonica.ms.linedataservice.enums;
 
+import br.com.telefonica.ms.linedataservice.exception.InvalidStatusLinhaException;
+
 import java.util.Arrays;
+import java.util.Objects;
 
 public enum StatusLinhaEnum {
     ATIVO,
     CANCELADO;
 
     public static StatusLinhaEnum fromKey(String key) {
-        if (key == null)
-            throw new IllegalArgumentException("Key value is null");
+        if (Objects.isNull(key))
+            throw new NullPointerException("Key cannot be null");
 
         return Arrays.stream(StatusLinhaEnum.values())
                 .filter(enumValue -> key
                         .equalsIgnoreCase(enumValue.name()))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("No enum constant for " + key));
+                .orElseThrow(() -> new InvalidStatusLinhaException("No enum constant for " + key));
     }
 
 }
