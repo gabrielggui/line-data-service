512cf160b539730d4e4c35da8d9cdfc511146858
Gabriel Guilherme <gabriel.guilherme@compasso.com.br>
2024-04-23 02:33:16 -0300

    feat: Create custom exception InvalidStatusLinhaException

---
+++ src/main/java/br/com/telefonica/ms/linedataservice/exception/InvalidStatusLinhaException.java
@@ -0,0 +1,7 @@
+package br.com.telefonica.ms.linedataservice.exception;
+
+public class InvalidStatusLinhaException extends RuntimeException {
+    public InvalidStatusLinhaException(String s) {
+        super(s);
+    }
+}
