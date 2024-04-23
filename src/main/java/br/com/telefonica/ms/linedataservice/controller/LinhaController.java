48efa9590296de3fb2cab874701e5edb4a70474a
Gabriel Guilherme <gabriel.guilherme@compasso.com.br>
2024-04-23 02:32:44 -0300

    feat: Add findLinhasByCpfCnpjAndStatusLinha method to LinhaController

--- src/main/java/br/com/telefonica/ms/linedataservice/controller/LinhaController.java
+++ src/main/java/br/com/telefonica/ms/linedataservice/controller/LinhaController.java
@@ -5,7 +5,6 @@ import br.com.telefonica.ms.linedataservice.service.LinhaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,16 +27,32 @@ public class LinhaController {
     /**
      * Retrieve a client's phone lines based on a CPF or CNPJ.
      *
-     * @author Gabriel Guilherme (gabriel.guilherme@compasso.com.br)
-     * @param cpfCnpj     the CPF or CNPJ of the client.
-     * @param statusLinha the status of the lines to be returned.
+     * @param cpfCnpj the CPF or CNPJ of the client.
      * @return The phone lines associated with a CPF or CNPJ.
+     * @author Gabriel Guilherme (gabriel.guilherme@compasso.com.br)
      */
     @Operation(summary = "Retrieve a client's phone lines based on a CPF or CNPJ.")
-    @GetMapping
-    public ResponseEntity<List<LinhaDTO>> getLinhas(
-            @NotBlank @RequestParam("cpf_cnpj") String cpfCnpj,
-            @RequestParam(required = false, name = "status_linha")
+    @GetMapping(params = "cpf_cnpj")
+    public ResponseEntity<List<LinhaDTO>> findLinhasByCpfCnpj(
+            @RequestParam("cpf_cnpj") String cpfCnpj) {
+        List<LinhaDTO> linhas = linhaService.findLinhasByCpfCnpj(cpfCnpj);
+
+        return ResponseEntity.ok().body(linhas);
+    }
+
+    /**
+     * Retrieves a client's phone lines based on their CPF or CNPJ and the phone line status.
+     *
+     * @param cpfCnpj     the client's CPF or CNPJ.
+     * @param statusLinha the status of the phone lines to be returned.
+     * @return The phone lines associated with a CPF or CNPJ.
+     * @author Gabriel Guilherme (gabriel.guilherme@compasso.com.br)
+     */
+    @Operation(summary = "Retrieves a client's phone lines based on their CPF or CNPJ and the phone line status.")
+    @GetMapping(params = {"cpf_cnpj", "status_linha"})
+    public ResponseEntity<List<LinhaDTO>> findLinhasByCpfCnpjAndStatusLinha(
+            @RequestParam("cpf_cnpj") String cpfCnpj,
+            @RequestParam(name = "status_linha")
             @Parameter(allowEmptyValue = true, description = "Available values: ATIVO, CANCELADO") String statusLinha) {
         List<LinhaDTO> linhas = linhaService.findLinhasByCpfCnpjAndStatusLinha(cpfCnpj, statusLinha);
 
