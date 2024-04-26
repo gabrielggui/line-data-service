3fae7a47a9ca2b03b48ac003f9f534edf8a11108
Gabriel Guilherme <gabriel.guilherme@compasso.com.br>
2024-04-26 14:48:47 -0300

    refactor: remove "fromKey" method

--- src/main/java/br/com/telefonica/ms/linedataservice/enums/StatusLinhaEnum.java
+++ src/main/java/br/com/telefonica/ms/linedataservice/enums/StatusLinhaEnum.java
@@ -1,23 +1,6 @@
 package br.com.telefonica.ms.linedataservice.enums;
 
-import br.com.telefonica.ms.linedataservice.exception.InvalidStatusLinhaException;
-
-import java.util.Arrays;
-import java.util.Objects;
-
 public enum StatusLinhaEnum {
     ATIVO,
     CANCELADO;
-
-    public static StatusLinhaEnum fromKey(String key) {
-        if (Objects.isNull(key))
-            throw new NullPointerException("Key cannot be null");
-
-        return Arrays.stream(StatusLinhaEnum.values())
-                .filter(enumValue -> key
-                        .equalsIgnoreCase(enumValue.name()))
-                .findFirst()
-                .orElseThrow(() -> new InvalidStatusLinhaException("No enum constant for " + key));
-    }
-
 }
