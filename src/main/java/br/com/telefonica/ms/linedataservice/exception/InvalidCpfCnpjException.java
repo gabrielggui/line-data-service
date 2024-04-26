d04f6d484d349c31fceb860942ab8fa17702bb6b
Gabriel Guilherme <gabriel.guilherme@compasso.com.br>
2024-04-26 14:53:07 -0300

    refactor: remove unused custom exceptions

--- src/main/java/br/com/telefonica/ms/linedataservice/exception/InvalidCpfCnpjException.java
+++
@@ -1,7 +0,0 @@
-package br.com.telefonica.ms.linedataservice.exception;
-
-public class InvalidCpfCnpjException extends RuntimeException{
-    public InvalidCpfCnpjException(String s){
-        super(s);
-    }
-}
--- src/main/java/br/com/telefonica/ms/linedataservice/exception/InvalidStatusLinhaException.java
+++
@@ -1,7 +0,0 @@
-package br.com.telefonica.ms.linedataservice.exception;
-
-public class InvalidStatusLinhaException extends RuntimeException {
-    public InvalidStatusLinhaException(String s) {
-        super(s);
-    }
-}
