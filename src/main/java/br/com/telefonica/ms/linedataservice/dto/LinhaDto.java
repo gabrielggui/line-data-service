252556ff3ae493dd7c3fe02661e58733c2e4f0de
Gabriel Guilherme <gabriel.guilherme@compasso.com.br>
2024-04-26 14:54:22 -0300

    refactor: rename "LinhaDTO" to "LinhaDto"

--- src/main/java/br/com/telefonica/ms/linedataservice/dto/LinhaDto.java
+++ src/main/java/br/com/telefonica/ms/linedataservice/dto/LinhaDto.java
@@ -2,12 +2,12 @@ package br.com.telefonica.ms.linedataservice.dto;
 
 import br.com.telefonica.ms.linedataservice.soap.stubs.Linha;
 
-public record LinhaDTO(String numeroLinha,
+public record LinhaDto(String numeroLinha,
                        String statusLinha,
                        String tipoLinha,
                        String origemLinha) {
 
-    public LinhaDTO(Linha linha) {
+    public LinhaDto(Linha linha) {
         this(linha.getNumeroLinha(),
                 linha.getStatusAssinatura().getDescricao(),
                 linha.getSistemaOrigem().getSigla(),
