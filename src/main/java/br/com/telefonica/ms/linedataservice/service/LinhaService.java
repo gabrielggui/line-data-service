fcdf1eb6cd225adc626ce7d05a4cf2233f96b0cf
Gabriel Guilherme <gabriel.guilherme@compasso.com.br>
2024-04-26 14:59:27 -0300

    refactor: modify methods in "LinhaService"

--- src/main/java/br/com/telefonica/ms/linedataservice/service/LinhaService.java
+++ src/main/java/br/com/telefonica/ms/linedataservice/service/LinhaService.java
@@ -1,12 +1,15 @@
 package br.com.telefonica.ms.linedataservice.service;
 
-import br.com.telefonica.ms.linedataservice.dto.LinhaDTO;
+import br.com.telefonica.ms.linedataservice.dto.LinhaDto;
+import br.com.telefonica.ms.linedataservice.enums.StatusLinhaEnum;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.List;
 
 public interface LinhaService {
 
-    List<LinhaDTO> findLinhasByCpfCnpj(String cpfCnpj);
+    List<LinhaDto> findLinhasByCpf(@CPF String cpfCnpj, StatusLinhaEnum statusLinha);
 
-    List<LinhaDTO> findLinhasByCpfCnpjAndStatusLinha(String CpfCnpj, String statusLinha);
+    List<LinhaDto> findLinhasByCnpj(@CNPJ String cpfCnpj, StatusLinhaEnum statusLinha);
 }
--- src/main/java/br/com/telefonica/ms/linedataservice/service/impl/LinhaServiceImpl.java
+++ src/main/java/br/com/telefonica/ms/linedataservice/service/impl/LinhaServiceImpl.java
@@ -1,18 +1,18 @@
 package br.com.telefonica.ms.linedataservice.service.impl;
 
-import br.com.telefonica.ms.linedataservice.dto.LinhaDTO;
+import br.com.telefonica.ms.linedataservice.dto.LinhaDto;
 import br.com.telefonica.ms.linedataservice.enums.StatusLinhaEnum;
-import br.com.telefonica.ms.linedataservice.exception.InvalidCpfCnpjException;
-import br.com.telefonica.ms.linedataservice.soap.stubs.Linha;
 import br.com.telefonica.ms.linedataservice.service.LinhaService;
-import br.com.telefonica.ms.linedataservice.util.CnpjUtil;
-import br.com.telefonica.ms.linedataservice.util.CpfUtil;
+import br.com.telefonica.ms.linedataservice.soap.stubs.Linha;
 import br.com.telefonica.ms.linedataservice.util.LinhaUtil;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
-import java.util.stream.Stream;
 
+@Validated
 @Service
 public class LinhaServiceImpl implements LinhaService {
 
@@ -22,41 +22,16 @@ public class LinhaServiceImpl implements LinhaService {
         this.linhaUtil = linhaUtil;
     }
 
-    /**
-     * Retrieve a client's phone lines based on a CPF or CNPJ.
-     *
-     * @param cpfCnpj the CPF or CNPJ of the client.
-     * @return The phone lines associated with a CPF or CNPJ.
-     * @author Gabriel Guilherme (gabriel.guilherme@compasso.com.br)
-     */
     @Override
-    public List<LinhaDTO> findLinhasByCpfCnpj(String cpfCnpj) {
-        if (!CpfUtil.isValidCpf(cpfCnpj) && !CnpjUtil.isValidCnpj(cpfCnpj))
-            throw new InvalidCpfCnpjException("Invalid CPF/CNPJ");
-
-        Stream<Linha> linhasStream = linhaUtil.findLinhasByCpfCnpj(cpfCnpj).stream();
-        return linhasStream.map(LinhaDTO::new).toList();
+    public List<LinhaDto> findLinhasByCpf(@CPF String cpf, StatusLinhaEnum statusLinha) {
+        List<Linha> linhas = linhaUtil.findLinhasByCpfCnpj(cpf, statusLinha);
+        return linhaUtil.mapLinhasToLinhasDtoList(linhas);
     }
 
-    /**
-     * Retrieves a client's phone lines based on a CPF or CNPJ and the line status.
-     *
-     * @param cpfCnpj     the client's CPF or CNPJ.
-     * @param statusLinha the status of the lines to be returned.
-     * @return The phone lines associated with a CPF or CNPJ, filtered by the phone line's status.
-     * @author Gabriel Guilherme (gabriel.guilherme@compasso.com.br)
-     */
     @Override
-    public List<LinhaDTO> findLinhasByCpfCnpjAndStatusLinha(String cpfCnpj, String statusLinha) {
-        if (!CpfUtil.isValidCpf(cpfCnpj) && !CnpjUtil.isValidCnpj(cpfCnpj))
-            throw new InvalidCpfCnpjException("Invalid CPF/CNPJ");
-
-        StatusLinhaEnum statusLinhaEnum = StatusLinhaEnum.fromKey(statusLinha);
-        Stream<Linha> linhasStream = linhaUtil.findLinhasByCpfCnpj(cpfCnpj).stream();
-
-        linhasStream = linhasStream.filter(linha -> statusLinhaEnum.name()
-                .equalsIgnoreCase(linha.getStatusAssinatura().getDescricao()));
-
-        return linhasStream.map(LinhaDTO::new).toList();
+    public List<LinhaDto> findLinhasByCnpj(@CNPJ String cnpj, StatusLinhaEnum statusLinha) {
+        List<Linha> linhas = linhaUtil.findLinhasByCpfCnpj(cnpj, statusLinha);
+        return linhaUtil.mapLinhasToLinhasDtoList(linhas);
     }
+
 }
